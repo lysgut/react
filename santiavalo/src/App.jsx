38eb28844c2bd2ed2cf2424254ed48e2a680b24a
lysgut <88504963+elixxi@users.noreply.github.com>
2023-09-01 16:21:44 -0300

    no entiendo nada

---
+++ santiavalo/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Card from "./componentes/cards"
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ santiavalo/src/componentes/cards.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+let piratas = [
+    {nombre: "Monkey D. Luffy",
+    cargo: "Capitán",
+    rango: "Younkou",
+    fruta: "Hito Hito no mi: Model Nika",
+    tripulacion: "Los Piratas de Sobrero de Paja"
+}
+]
+function Hola (){
+
+        return <div><h1>Nombre:{elem.nombre}</h1></div>
+    }
+
+
+export default Hola
